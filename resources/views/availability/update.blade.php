87d2bb00f851cd7d63fe478c2ab8f46b3f4d74c8
Tomelinski <59152676+Tomelinski@users.noreply.github.com>
2021-12-06 17:47:39 -0500

    updated availaility update apge

--- resources/views/availability/update.blade.php
+++ resources/views/availability/update.blade.php
@@ -14,10 +14,34 @@ $days = [
     <table class="table align-center col-10">
         <tbody>
             @foreach ($days as $day) 
+            <datalist id="times">
+                <option value="08:00:00">
+                <option value="09:00:00">
+                <option value="10:00:00">
+                <option value="11:00:00">
+                <option value="12:00:00">
+                <option value="13:00:00">
+                <option value="14:00:00">
+                <option value="15:00:00">
+                <option value="16:00:00">
+                <option value="17:00:00">
+                <option value="18:00:00">
+                <option value="19:00:00">
+                <option value="20:00:00">
+            </datalist>
             <tr class="text-center">
-                <td>{{$day}}</td>
-                <td><label for="{{$day}}-start"> Start Time: </label> <input type="time" id="{{$day}}-start" name="{{$day}}-start" min="08:00" max="20:00"></td>
-                <td><label for="{{$day}}-end"> End Time: </label> <input type="time" id="{{$day}}-end" name="{{$day}}-end" min="08:00" max="20:00"></td>
+                <td>
+                    {{$day}}
+                </td>
+                <td>
+                    <label for="{{$day}}-start"> Start Time: </label> 
+                    <input list="times" id="{{$day}}-start" name="{{$day}}-start">
+                </td>
+                
+                <td>
+                    <label for="{{$day}}-end"> End Time: </label> 
+                    <input list="times" id="{{$day}}-end" name="{{$day}}-end">
+                </td>
             </tr>
             @endforeach
         </tbody>
