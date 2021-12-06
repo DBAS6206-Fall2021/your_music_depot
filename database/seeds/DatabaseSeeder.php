8a319eafd053e91088010c70d700e556a91891bc
Alejandro <60989886+ed3642@users.noreply.github.com>
2021-12-06 15:29:01 -0500

    initial commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,19 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(RoomTypesSeeder::class);
-        // $this->command->info('room_types table seeded!');
+        $this->call(RoomTypesSeeder::class);
+        $this->command->info('room_types table seeded!');
 
-        // $this->call(UserTypesSeeder::class);
-        // $this->command->info('user_types table seeded!');
+        $this->call(UserTypesSeeder::class);
+        $this->command->info('user_types table seeded!');
 
-        // $this->call(SkillLevelsSeeder::class);
-        // $this->command->info('skill_levels table seeded!');
+        $this->call(SkillLevelsSeeder::class);
+        $this->command->info('skill_levels table seeded!');
 
-        // $this->call(InstrumentsTableSeeder::class);
-        // $this->command->info('instruments table seeded!');
+        $this->call(InstrumentsTableSeeder::class);
+        $this->command->info('instruments table seeded!');
 
-        // $this->call(LessonTypesTableSeeder::class);
-        // $this->command->info('lesson_types table seeded!');
+        $this->call(LessonTypesTableSeeder::class);
+        $this->command->info('lesson_types table seeded!');
     }
 }
