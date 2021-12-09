37218da34a3ea335833681a48f9065d3eeb15ab7
lucas-mawdsley <75540418+lucas-mawdsley@users.noreply.github.com>
2021-12-08 21:42:09 -0500

    New Landing Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,23 @@
 
 @section('content')
 
-    <div class="title m-b-md">
-        DBASE 6202 HOME PAGE
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <h1 class="cover-heading text-center">Your Music Depot</h1>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <p class="lead text-center">Make an account and book a lesson today!</p>
+        </div>
+        <hr/>
+        <div class="row d-flex justify-content-center">
+            <p class="text-center">
+                <strong>Your Music Depot</strong> is your one-stop-shop for all of your childrens' musical needs.<br/>
+                <br/>
+                From guitar to Saxophone, beginner to expert, our variety of instructors can help develop any musical talent.<br/>
+                <br/>
+                Need supplies? Swing by <strong>Your Music Depot's</strong> in-house music shop. Chances are we'll have what you need on hand.
+            </p>
+        </div>
+        <hr/>
     </div>
-</div>
 @endsection
